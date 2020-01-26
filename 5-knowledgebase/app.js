0ffb55af201f4599c093699ecea1ea55c99e411f
jemmeli <jemmeli84@gmail.com>
2020-01-26 21:44:40 +0100

    first commit

---
+++ 5-knowledgebase/app.js
@@ -0,0 +1,53 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var db = mongoose.connection;
+mongoose.connect('mongodb://localhost/knowledgebase');
+
+var routes = require('./routes/index');
+var articles = require('./routes/articles');
+var categories = require('./routes/categories');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
+app.use(express.static(path.join(__dirname, 'controllers')));
+
+
+
+
+app.use('/', routes);
+app.use('/articles', articles);
+app.use('/categories', categories);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ 5-knowledgebase/controllers/articles.controllers.js
@@ -0,0 +1,85 @@
+angular.module("kB")
+.controller('ArticlesCtrl', ['$scope','$http', function($scope,$http){
+
+	$http.get('/articles').then(function(data){
+		$scope.articles = data.data;
+	});
+
+}])
+
+.controller('ArticlesCategoryCtrl', ['$scope','$http','$routeParams', function($scope,$http,$routeParams){
+
+	$http.get('/articles/category/'+$routeParams.category).then(function(data){
+		$scope.cat_articles = data.data;
+		$scope.category = $routeParams.category;
+	});
+
+}])
+
+.controller('ArticleDetailsCtrl', ['$scope','$http','$routeParams','$location', function($scope,$http,$routeParams,$location){
+
+	$http.get('/articles/'+$routeParams.id).then(function(data){
+		$scope.article = data.data;
+	});
+
+	$scope.removeArticle = function(){
+		$http.delete('/articles/' + $routeParams.id).then(function(data){
+			console.log(data);
+		});
+
+		$location.path('/articles');
+	};
+
+
+}])
+
+.controller('ArticleCreateCtrl', ['$scope','$http', '$routeParams', '$location', 
+									function($scope, $http, $routeParams, $location){
+
+	$http.get('/categories').then(function(data){
+		$scope.categories = data.data;
+	});
+
+	$scope.addArticle = function(){
+		var data = {
+			title: $scope.title,
+			body: $scope.body,
+			category: $scope.category
+		}
+
+		$http.post('/articles', data).then(function(data, status) {
+			console.log(status);
+		});
+
+		$location.path('/articles');
+	};
+}])
+
+.controller('ArticleEditCtrl', ['$scope','$http', '$routeParams', '$location', 
+									function($scope, $http, $routeParams, $location){
+	$http.get('/categories').then(function(data){
+		$scope.categories = data.data;
+	});
+
+	$http.get('/articles/' + $routeParams.id).then(function(data){
+		$scope.article = data.data;
+	});
+
+	$scope.updateArticle = function(){
+		var data = {
+			id: $routeParams.id,
+			title: $scope.article.title,
+			body: $scope.article.body,
+			category: $scope.article.category
+		}
+
+		$http.put('/articles', data).then(function(data, status) {
+			console.log(status);
+		});
+
+		$location.path('/articles');
+	};
+}]);
+
+
+;
